61f9aa1ac43f9f5f30de4a4a183ab699516f68c8
ProdPreva1l <prod.preva1l@gmail.com>
2024-03-21 14:40:33 +1100

    Initial Commit

--- database/AppStorage.js
+++ database/AppStorage.js
@@ -1,10 +1,15 @@
 import {MongoClient} from "mongodb";
 import {mongo_uri} from "../config/Config.js";
+import logger from "../utils/Logger.js";
 
-export class AppStorage {
-    constructor(uri) {
+const client = new MongoClient(mongo_uri);
 
+async function initiate() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ping:1});
+        logger.info("Connected to FreeDG storage MongoDB")
+    } finally {
+        await client.close();
     }
-
-    client = new MongoClient(mongo_uri);
 }
